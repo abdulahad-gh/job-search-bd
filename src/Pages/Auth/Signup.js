17f163133fbe189a336234e53fb412a13c292e1c
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-03-18 10:06:19 +0000

    create signup page, and use react-hook-form to collect data

--- src/Pages/Auth/Signup.js
+++ src/Pages/Auth/Signup.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useForm, useWatch } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import '../../App.css'
+
 
 const Signup = () => {
+  const {handleSubmit,register,formState:{errors},control} = useForm()
+const password = useWatch({control,name:'password'})
+const confirmPassword = useWatch({control,name:'confirmPassword'})
+const [disabled, setDisabled] = useState(true)
+
+useEffect(()=>{
+if(password !== undefined && password !== "" && confirmPassword !== undefined && confirmPassword !== '' && password === confirmPassword){
+setDisabled(false)
+}else{
+setDisabled(true)
+}
+},[password,confirmPassword])
+  const onSubmit = data => console.log(data)
     return (
-        <div>
-          Signup
+        <div className='form sign-up'>
+          <h3>Create Account</h3>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input placeholder='firstname' type='text' {...register('firstName',{required:"first name don't blank!",maxLength:20})} />
+            {errors.firstName && <p role='alert'>{errors.firstName.message}</p>}
+            <input placeholder='lastname' type='text' {...register('lastName',{required:"last name don't blank!", maxLength:20})} />
+            {errors.lastName && <p role='alert'>{errors.lastName.message}</p>}
+            <input placeholder='example@mail.com' type='email' {...register('email',{required:'must field'})} />
+            {errors.email && <p role='alert'>{errors.email.message}</p>}
+            <select {...register('gender')}>  
+              <option value='male'>male</option>
+              <option value='female'>female</option>
+              <option value='other'>other</option>
+            </select>
+            <input placeholder='password' type='password' {...register('password')} />
+            <input  placeholder='confirmPassword' type='password' {...register('confirmPassword')} />
+            <input disabled={disabled} type='submit' value='signup' />
+            <p>already Account? <Link to='/login'>Login</Link></p>
+          </form>
+          
         </div>
     );
 };
